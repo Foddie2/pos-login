4f140b13d30b1b1da97b9492d5ed5a4d4bd2bef8
Foddie2 <Bensonagala@live.com>
2022-07-13 23:06:11 +0300

    component structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react'
 // import ReactDOM from 'react-dom'
+import Login from './components/Login'
 
 function App() {
   return (
     <div>
     
-    <div>
-    <h1>Hello</h1>
+    <Login/>
+
 
     
     
-    </div>
     </div>
    
    
   );
 }
+
+
 
 export default App;
